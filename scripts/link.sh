929fbf2eff5bdcd153bbe88c53ff912e1724bdb0
Christopher Kahn <christopher.kahn@creditkarma.com>
2021-06-21 19:18:40 +0100

    Updated some stuff

--- scripts/link.sh
+++ scripts/link.sh
@@ -10,10 +10,10 @@ fi
 mkdir -p $HOME/.local/bin
 
 # Terminal stuff
-test -L $HOME/.config/alacritty     || ln -s $DOTFILES/etc/alacritty $HOME/.config/alacritty
-test -L $HOME/.tmux.conf            || ln -s $DOTFILES/etc/.tmux.conf $HOME/.tmux.conf
-test -L $HOME/.config/fish          || ln -s $DOTFILES/etc/fish $HOME/.config/fish
-test -L $HOME/.config/starship.toml || ln -s $DOTFILES/etc/starship.toml $HOME/.config/starship.toml
+test -L $HOME/.config/alacritty        || ln -s $DOTFILES/etc/alacritty $HOME/.config/alacritty
+test -L $HOME/.tmux.conf               || ln -s $DOTFILES/etc/.tmux.conf $HOME/.tmux.conf
+test -L $HOME/.config/fish/config.fish || ln -s $DOTFILES/etc/fish/config.fish $HOME/.config/fish/config.fish
+test -L $HOME/.config/starship.toml    || ln -s $DOTFILES/etc/starship.toml $HOME/.config/starship.toml
 
 # Git config
 test -L $HOME/.gitconfig            || ln -s $DOTFILES/etc/.gitconfig $HOME/.gitconfig
@@ -27,8 +27,10 @@ test -L $HOME/.config/skhd      || ln -s $DOTFILES/etc/skhd $HOME/.config/skhd
 test -L $HOME/.config/limelight || ln -s $DOTFILES/etc/limelight $HOME/.config/limelight
 test -L $HOME/.config/spacebar  || ln -s $DOTFILES/etc/spacebar $HOME/.config/spacebar
 
-# Mail
-test -L $HOME/.mbsyncrc         || ln -s $DOTFILES/etc/.mbsyncrc $HOME/.mbsyncrc
-test -L $HOME/.config/mutt      || ln -s $DOTFILES/etc/mutt $HOME/.config/mutt
+if [[ "$1" == "home" ]]; then
+    # Mail
+    test -L $HOME/.mbsyncrc         || ln -s $DOTFILES/etc/.mbsyncrc $HOME/.mbsyncrc
+    test -L $HOME/.config/mutt      || ln -s $DOTFILES/etc/mutt $HOME/.config/mutt
+fi
 
 test -L $HOME/.local/bin/gaps   || ln -s $DOTFILES/scripts/gaps $HOME/.local/bin/gaps
